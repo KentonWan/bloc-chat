f8bef6f271c5996372cfb8dec458cd750957fb50
Kenton Wan <kenton.wan@gmail.com>
2018-06-18 16:21:08 -0700

    Assignment 2 List Chat Rooms: Completed

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+var config = {
+    apiKey: "AIzaSyAgtJqQDyrWpyTIeQcSSXTC5DuWMSpeJrc",
+    authDomain: "bloc-chat-cd3f8.firebaseapp.com",
+    databaseURL: "https://bloc-chat-cd3f8.firebaseio.com",
+    projectId: "bloc-chat-cd3f8",
+    storageBucket: "bloc-chat-cd3f8.appspot.com",
+    messagingSenderId: "307760294352"
+  };
+  firebase.initializeApp(config);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Bloc Chat</h1>
+          </header>
+        <nav className="room-navigation">
+          <RoomList firebase={firebase}/>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
