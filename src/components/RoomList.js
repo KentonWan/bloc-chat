41ce1a8ad142cfa86e4074e364ba7fedf4d8d45d
Kenton Wan <kenton.wan@gmail.com>
2018-06-18 16:49:17 -0700

    Checkpoint 3 Add Chat Room: Completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,7 +4,8 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      value: ''
     };
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
@@ -16,15 +17,37 @@ class RoomList extends Component {
       this.setState({rooms: this.state.rooms.concat(room )});
     });
   }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
+  createRoom() {
+    let newRoomName = this.state.value;
+    console.log(newRoomName);
+    this.roomsRef.push({
+      name: newRoomName
+    });
+
+  }
+
   render() {
     return (
-      <ul>
-      {
-        this.state.rooms.map((room,index) =>
-          <li key={index} className="room-number">{room.name}</li>
-        )
-      }
-      </ul>
+      <div>
+        <ul>
+        {
+          this.state.rooms.map((room,index) =>
+            <li key={index} className="room-number">{room.name}</li>
+          )
+        }
+        </ul>
+        <form onSubmit={()=>this.createRoom()}>
+          <label>
+            Chat Room Name:  <input type="text" value={this.state.value} onChange={(e)=>this.handleChange(e)} />
+          </label>
+          <input type="submit" value="Submit"/>
+        </form>
+      </div>
     );
   }
 }
