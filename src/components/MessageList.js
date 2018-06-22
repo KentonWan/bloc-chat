830e872b9b11178df87cc4a13dd491094399086f
Kenton Wan <kenton.wan@gmail.com>
2018-06-22 12:30:24 -0700

    Checkpoint 4: Revision

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -5,17 +5,10 @@ class MessageList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: [{
-        username: "",
-        content: "",
-        sentAt: "",
-        roomId: ""
-      }],
+      messages: [],
 
-    };
+    }
     this.messagesRef = this.props.firebase.database().ref('messages');
-    this.messagesRef.push({
-      sentAt: this.props.firebase.database.ServerValue.TIMESTAMP});
     }
 
   componentDidMount () {
@@ -24,15 +17,15 @@ class MessageList extends Component {
       message.key = snapshot.key;
       this.setState({messages: this.state.messages.concat(message )});
     });
-
-
   }
 
   render() {
     return (
       <div className="messages">
-        {this.state.messages.filter(message => message.roomId === this.props.activeRoom).map((message,index)=>
-        <div className={index}>{message.content}</div>)}
+        {
+            this.state.messages.filter(message => message.roomId === this.props.activeRoom).map((message,index)=> { return <div className={index}>{message.content}</div>
+            })
+        }
       </div>
     )
   }
